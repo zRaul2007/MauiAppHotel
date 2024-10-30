7135bbf6232c256f3b977001cd37e7fa5e4c5f83
Raul Pedrogan <rpm2007@uol.com.br>
2024-10-30 11:54:13 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Height = 600;
+            window.Width = 400;
+
+            return window;
+
+        }
+    }
+}
