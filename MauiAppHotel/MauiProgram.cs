bca962af2a730bd8c216a3b0c0fb7773cea51007
Raul Pedrogan <rpm2007@uol.com.br>
2024-10-30 14:16:49 -0300

    Forma do App

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("PoiretOne-Regular.ttf", "PoiretOne");
                 });
 
 #if DEBUG
